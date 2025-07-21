vvhh2002/libwebsockets

asynchronous dns for ipv4 and ipv6
This adds the option to have lws do its own dns resolution on
the event loop, without blocking.  Existing implementations get
the name resolution done by the libc, which is blocking.  In
the case you are opening client connections but need to carefully
manage latency, another connection opening and doing the name
resolution becomes a big problem.

Currently it supports

 - ipv4 / A records
 - ipv6 / AAAA records
 - ipv4-over-ipv6 ::ffff:1.2.3.4 A record promotion for ipv6
 - only one server supported over UDP :53
 - nameserver discovery on linux, windows, freertos

It also has some nice advantages

 - lws-style paranoid response parsing
 - random unique tid generation to increase difficulty of poisoning
 - it's really integrated with the lws event loop, it does not spawn
   threads or use the libc resolver, and of course no blocking at all
 - platform-specific server address capturing (from /etc/resolv.conf
   on linux, windows apis on windows)
 - it has LRU caching
 - piggybacking (multiple requests before the first completes go on
   a list on the first request, not spawn multiple requests)
 - observes TTL in cache
 - TTL and timeout use lws_sul timers on the event loop
 - ipv6 pieces only built if cmake LWS_IPV6 enabled

lib/core-net/close.c
@@ -133,6 +133,8 @@ lws_close_trans_q_leader(struct lws_dll2 *d, void *user)
 void
 lws_inform_client_conn_fail(struct lws *wsi, void *arg, size_t len)
 {
+	lws_addrinfo_clean(wsi);
+
 	if (wsi->already_did_cce)
 		return;
 
@@ -148,6 +150,22 @@ lws_inform_client_conn_fail(struct lws *wsi, void *arg, size_t len)
 				wsi->user_space, arg, len);
 }
 #endif
+
+void
+lws_addrinfo_clean(struct lws *wsi)
+{
+#if defined(LWS_WITH_CLIENT)
+	if (!wsi->dns_results)
+		return;
+
+#if defined(LWS_WITH_ASYNC_DNS)
+	lws_async_dns_freeaddrinfo(wsi->dns_results);
+#else
+	freeaddrinfo((struct addrinfo *)wsi->dns_results);
+#endif
+	wsi->dns_results = NULL;
+#endif
+}
 
 void
 __lws_close_free_wsi(struct lws *wsi, enum lws_close_status reason,
@@ -175,6 +193,8 @@ __lws_close_free_wsi(struct lws *wsi, enum lws_close_status reason,
 #if defined(LWS_WITH_CLIENT)
 
 	lws_free_set_NULL(wsi->cli_hostname_copy);
+
+	lws_addrinfo_clean(wsi);
 
 	/*
 	 * if we have wsi in our transaction queue, if we are closing we
@@ -317,6 +337,7 @@ __lws_close_free_wsi(struct lws *wsi, enum lws_close_status reason,
 	}
 
 	if (lwsi_state(wsi) == LRS_WAITING_CONNECT ||
+	    lwsi_state(wsi) == LRS_WAITING_DNS ||
 	    lwsi_state(wsi) == LRS_H1C_ISSUE_HANDSHAKE)
 		goto just_kill_connection;
 
@@ -347,6 +368,10 @@ __lws_close_free_wsi(struct lws *wsi, enum lws_close_status reason,
 
 just_kill_connection:
 
+#if defined(LWS_WITH_ASYNC_DNS)
+	lws_async_dns_cancel(wsi);
+#endif
+
 #if defined(LWS_WITH_HTTP_PROXY)
 	if (wsi->http.buflist_post_body)
 		lws_buflist_destroy_all_segments(&wsi->http.buflist_post_body);
@@ -371,6 +396,7 @@ just_kill_connection:
 
 #if defined(LWS_WITH_CLIENT)
 	if ((lwsi_state(wsi) == LRS_WAITING_SERVER_REPLY ||
+	     lwsi_state(wsi) == LRS_WAITING_DNS ||
 	     lwsi_state(wsi) == LRS_WAITING_CONNECT) &&
 	     !wsi->already_did_cce && wsi->protocol)
 		lws_inform_client_conn_fail(wsi,

lib/core-net/vhost.c
@@ -444,6 +444,9 @@ lws_create_vhost(struct lws_context *context,
     defined(LWS_WITH_CLIENT) && defined(LWS_HAVE_GETENV)
 	char *p;
 #endif
+#if defined(LWS_WITH_ASYNC_DNS)
+	extern struct lws_protocols lws_async_dns_protocol;
+#endif
 	int n;
 
 	if (!vh)
@@ -558,6 +561,7 @@ lws_create_vhost(struct lws_context *context,
 	/*
 	 * give the vhost a unified list of protocols including:
 	 *
+	 * - internal, async_dns if enabled (first vhost only)
 	 * - internal, abstracted ones
 	 * - the ones that came from plugins
 	 * - his user protocols
@@ -591,6 +595,16 @@ lws_create_vhost(struct lws_context *context,
 	for (n = 0; n < abs_pcol_count; n++) {
 		memcpy(&lwsp[m++], available_abstract_protocols[n],
 		       sizeof(*lwsp));
+		vh->count_protocols++;
+	}
+#endif
+	/*
+	 * 3: async dns protocol (first vhost only)
+	 */
+#if defined(LWS_WITH_ASYNC_DNS)
+	if (!context->vhost_list) {
+		memcpy(&lwsp[m++], &lws_async_dns_protocol,
+		       sizeof(struct lws_protocols));
 		vh->count_protocols++;
 	}
 #endif
@@ -763,6 +777,7 @@ lws_create_vhost(struct lws_context *context,
 		goto bail1;
 	}
 #endif
+	n = !!context->vhost_list;
 
 	while (1) {
 		if (!(*vh1)) {
@@ -771,6 +786,11 @@ lws_create_vhost(struct lws_context *context,
 		}
 		vh1 = &(*vh1)->vhost_next;
 	};
+
+#if defined(LWS_WITH_ASYNC_DNS)
+	if (!n && lws_async_dns_init(context))
+		goto bail1;
+#endif
 
 	/* for the case we are adding a vhost much later, after server init */
 

lib/core/context.c
@@ -639,6 +639,10 @@ lws_context_destroy3(struct lws_context *context)
 #endif
 	}
 
+#if defined(LWS_WITH_ASYNC_DNS)
+	lws_async_dns_deinit(&context->async_dns);
+#endif
+
 	if (context->pt[0].fds)
 		lws_free_set_NULL(context->pt[0].fds);
 #endif

lib/core/private-lib-core.h
@@ -259,34 +259,72 @@ struct lws_deferred_free
  */
 
 struct lws_context {
-	time_t last_ws_ping_pong_check_s;
-	lws_usec_t time_up; /* monotonic */
+ #if defined(LWS_WITH_SERVER)
+	char canonical_hostname[96];
+ #endif
+
 #if defined(LWS_WITH_FILE_OPS)
-	const struct lws_plat_file_ops *fops;
 	struct lws_plat_file_ops fops_platform;
 #endif
-	struct lws_context **pcontext_finalize;
 
-#if defined(LWS_WITH_TLS)
-	const struct lws_tls_ops *tls_ops;
+#if defined(LWS_WITH_ZIP_FOPS)
+	struct lws_plat_file_ops fops_zip;
 #endif
 
-	const char *username, *groupname;
+#if defined(LWS_WITH_NETWORK)
+
+	struct lws_context_per_thread pt[LWS_MAX_SMP];
 
 #if defined(LWS_WITH_HTTP2)
 	struct http2_settings set;
 #endif
-#if defined(LWS_WITH_ZIP_FOPS)
-	struct lws_plat_file_ops fops_zip;
-#endif
-#if defined(LWS_WITH_NETWORK)
-	struct lws_context_per_thread pt[LWS_MAX_SMP];
+
 #if defined(LWS_WITH_SERVER_STATUS)
 	struct lws_conn_stats conn_stats;
 #endif
+#if LWS_MAX_SMP > 1
+	struct lws_mutex_refcount mr;
+#endif
+
+#if defined(LWS_WITH_LIBEV)
+	struct lws_context_eventlibs_libev ev;
+#endif
+#if defined(LWS_WITH_LIBUV)
+	struct lws_context_eventlibs_libuv uv;
+#endif
+#if defined(LWS_WITH_LIBEVENT)
+	struct lws_context_eventlibs_libevent event;
+#endif
+
+#if defined(LWS_WITH_TLS)
+	struct lws_context_tls tls;
+#endif
+
+#if defined(LWS_WITH_ASYNC_DNS)
+	lws_async_dns_t		async_dns;
+#endif
+
+	/* pointers */
+
 	struct lws_vhost *vhost_list;
 	struct lws_vhost *no_listener_vhost_list;
 	struct lws_vhost *vhost_pending_destruction_list;
+	struct lws_context **pcontext_finalize;
+	const char *username, *groupname;
+
+#if defined(LWS_WITH_SERVER)
+	const char *server_string;
+#endif
+
+	struct lws_event_loop_ops *event_loop_ops;
+
+#if defined(LWS_WITH_FILE_OPS)
+	const struct lws_plat_file_ops *fops;
+#endif
+
+#if defined(LWS_WITH_TLS)
+	const struct lws_tls_ops *tls_ops;
+#endif
 #if defined(LWS_WITH_PLUGINS)
 	struct lws_plugin *plugin_list;
 #endif
@@ -297,10 +335,7 @@ struct lws_context {
 	struct lws **lws_lookup;
 
 #endif
-#endif
-#if LWS_MAX_SMP > 1
-	struct lws_mutex_refcount mr;
-#endif
+#endif /* NETWORK */
 
 #if defined(LWS_AMAZON_RTOS)
 	mbedtls_entropy_context mec;
@@ -329,36 +364,12 @@ struct lws_context {
 #endif
 	void (*eventlib_signal_cb)(void *event_lib_handle, int signum);
 
+	time_t last_ws_ping_pong_check_s;
+	lws_usec_t time_up; /* monotonic */
+
 #if defined(LWS_HAVE_SYS_CAPABILITY_H) && defined(LWS_HAVE_LIBCAP)
 	cap_value_t caps[4];
 	char count_caps;
-#endif
-
-#if defined(LWS_WITH_NETWORK)
-#if defined(LWS_WITH_LIBEV)
-	struct lws_context_eventlibs_libev ev;
-#endif
-#if defined(LWS_WITH_LIBUV)
-	struct lws_context_eventlibs_libuv uv;
-#endif
-#if defined(LWS_WITH_LIBEVENT)
-	struct lws_context_eventlibs_libevent event;
-#endif
-	struct lws_event_loop_ops *event_loop_ops;
-#endif
-
-#if defined(LWS_WITH_TLS) && defined(LWS_WITH_NETWORK)
-	struct lws_context_tls tls;
-#endif
-
-#if defined(LWS_WITH_SERVER)
-	char canonical_hostname[128];
-	const char *server_string;
-#endif
-
-#ifdef LWS_LATENCY
-	unsigned long worst_latency;
-	char worst_latency_info[256];
 #endif
 
 #if defined(LWS_PLAT_FREERTOS)

lib/plat/freertos/private-lib-plat-freertos.h
@@ -55,6 +55,9 @@ gai_strerror(int);
 
 #if defined(LWS_AMAZON_RTOS)
  #include "FreeRTOS.h"
+#if defined(LWS_WITH_ASYNC_DNS)
+ #include "FreeRTOS_IP.h"
+#endif
  #include "timers.h"
  #include <esp_attr.h>
 #else

open-vela/apps_netutils_libwebsockets

lib/core-net/close.c
@@ -133,6 +133,8 @@ lws_close_trans_q_leader(struct lws_dll2 *d, void *user)
 void
 lws_inform_client_conn_fail(struct lws *wsi, void *arg, size_t len)
 {
+	lws_addrinfo_clean(wsi);
+
 	if (wsi->already_did_cce)
 		return;
 
@@ -148,6 +150,22 @@ lws_inform_client_conn_fail(struct lws *wsi, void *arg, size_t len)
 				wsi->user_space, arg, len);
 }
 #endif
+
+void
+lws_addrinfo_clean(struct lws *wsi)
+{
+#if defined(LWS_WITH_CLIENT)
+	if (!wsi->dns_results)
+		return;
+
+#if defined(LWS_WITH_SYS_ASYNC_DNS)
+	lws_async_dns_freeaddrinfo(wsi->dns_results);
+#else
+	freeaddrinfo((struct addrinfo *)wsi->dns_results);
+#endif
+	wsi->dns_results = NULL;
+#endif
+}
 
 void
 __lws_close_free_wsi(struct lws *wsi, enum lws_close_status reason,
@@ -175,6 +193,8 @@ __lws_close_free_wsi(struct lws *wsi, enum lws_close_status reason,
 #if defined(LWS_WITH_CLIENT)
 
 	lws_free_set_NULL(wsi->cli_hostname_copy);
+
+	lws_addrinfo_clean(wsi);
 
 	/*
 	 * if we have wsi in our transaction queue, if we are closing we
@@ -317,6 +337,7 @@ __lws_close_free_wsi(struct lws *wsi, enum lws_close_status reason,
 	}
 
 	if (lwsi_state(wsi) == LRS_WAITING_CONNECT ||
+	    lwsi_state(wsi) == LRS_WAITING_DNS ||
 	    lwsi_state(wsi) == LRS_H1C_ISSUE_HANDSHAKE)
 		goto just_kill_connection;
 
@@ -347,6 +368,10 @@ __lws_close_free_wsi(struct lws *wsi, enum lws_close_status reason,
 
 just_kill_connection:
 
+#if defined(LWS_WITH_SYS_ASYNC_DNS)
+	lws_async_dns_cancel(wsi);
+#endif
+
 #if defined(LWS_WITH_HTTP_PROXY)
 	if (wsi->http.buflist_post_body)
 		lws_buflist_destroy_all_segments(&wsi->http.buflist_post_body);
@@ -371,6 +396,7 @@ just_kill_connection:
 
 #if defined(LWS_WITH_CLIENT)
 	if ((lwsi_state(wsi) == LRS_WAITING_SERVER_REPLY ||
+	     lwsi_state(wsi) == LRS_WAITING_DNS ||
 	     lwsi_state(wsi) == LRS_WAITING_CONNECT) &&
 	     !wsi->already_did_cce && wsi->protocol)
 		lws_inform_client_conn_fail(wsi,

lib/core-net/vhost.c
@@ -444,6 +444,9 @@ lws_create_vhost(struct lws_context *context,
     defined(LWS_WITH_CLIENT) && defined(LWS_HAVE_GETENV)
 	char *p;
 #endif
+#if defined(LWS_WITH_SYS_ASYNC_DNS)
+	extern struct lws_protocols lws_async_dns_protocol;
+#endif
 	int n;
 
 	if (!vh)
@@ -558,6 +561,7 @@ lws_create_vhost(struct lws_context *context,
 	/*
 	 * give the vhost a unified list of protocols including:
 	 *
+	 * - internal, async_dns if enabled (first vhost only)
 	 * - internal, abstracted ones
 	 * - the ones that came from plugins
 	 * - his user protocols
@@ -591,6 +595,16 @@ lws_create_vhost(struct lws_context *context,
 	for (n = 0; n < abs_pcol_count; n++) {
 		memcpy(&lwsp[m++], available_abstract_protocols[n],
 		       sizeof(*lwsp));
+		vh->count_protocols++;
+	}
+#endif
+	/*
+	 * 3: async dns protocol (first vhost only)
+	 */
+#if defined(LWS_WITH_SYS_ASYNC_DNS)
+	if (!context->vhost_list) {
+		memcpy(&lwsp[m++], &lws_async_dns_protocol,
+		       sizeof(struct lws_protocols));
 		vh->count_protocols++;
 	}
 #endif
@@ -763,6 +777,7 @@ lws_create_vhost(struct lws_context *context,
 		goto bail1;
 	}
 #endif
+	n = !!context->vhost_list;
 
 	while (1) {
 		if (!(*vh1)) {
@@ -771,6 +786,11 @@ lws_create_vhost(struct lws_context *context,
 		}
 		vh1 = &(*vh1)->vhost_next;
 	};
+
+#if defined(LWS_WITH_SYS_ASYNC_DNS)
+	if (!n && lws_async_dns_init(context))
+		goto bail1;
+#endif
 
 	/* for the case we are adding a vhost much later, after server init */
 

lib/core/context.c
@@ -639,6 +639,10 @@ lws_context_destroy3(struct lws_context *context)
 #endif
 	}
 
+#if defined(LWS_WITH_SYS_ASYNC_DNS)
+	lws_async_dns_deinit(&context->async_dns);
+#endif
+
 	if (context->pt[0].fds)
 		lws_free_set_NULL(context->pt[0].fds);
 #endif

lib/core/private-lib-core.h
@@ -259,34 +259,72 @@ struct lws_deferred_free
  */
 
 struct lws_context {
-	time_t last_ws_ping_pong_check_s;
-	lws_usec_t time_up; /* monotonic */
+ #if defined(LWS_WITH_SERVER)
+	char canonical_hostname[96];
+ #endif
+
 #if defined(LWS_WITH_FILE_OPS)
-	const struct lws_plat_file_ops *fops;
 	struct lws_plat_file_ops fops_platform;
 #endif
-	struct lws_context **pcontext_finalize;
 
-#if defined(LWS_WITH_TLS)
-	const struct lws_tls_ops *tls_ops;
+#if defined(LWS_WITH_ZIP_FOPS)
+	struct lws_plat_file_ops fops_zip;
 #endif
 
-	const char *username, *groupname;
+#if defined(LWS_WITH_NETWORK)
+
+	struct lws_context_per_thread pt[LWS_MAX_SMP];
 
 #if defined(LWS_WITH_HTTP2)
 	struct http2_settings set;
 #endif
-#if defined(LWS_WITH_ZIP_FOPS)
-	struct lws_plat_file_ops fops_zip;
-#endif
-#if defined(LWS_WITH_NETWORK)
-	struct lws_context_per_thread pt[LWS_MAX_SMP];
+
 #if defined(LWS_WITH_SERVER_STATUS)
 	struct lws_conn_stats conn_stats;
 #endif
+#if LWS_MAX_SMP > 1
+	struct lws_mutex_refcount mr;
+#endif
+
+#if defined(LWS_WITH_LIBEV)
+	struct lws_context_eventlibs_libev ev;
+#endif
+#if defined(LWS_WITH_LIBUV)
+	struct lws_context_eventlibs_libuv uv;
+#endif
+#if defined(LWS_WITH_LIBEVENT)
+	struct lws_context_eventlibs_libevent event;
+#endif
+
+#if defined(LWS_WITH_TLS)
+	struct lws_context_tls tls;
+#endif
+
+#if defined(LWS_WITH_SYS_ASYNC_DNS)
+	lws_async_dns_t		async_dns;
+#endif
+
+	/* pointers */
+
 	struct lws_vhost *vhost_list;
 	struct lws_vhost *no_listener_vhost_list;
 	struct lws_vhost *vhost_pending_destruction_list;
+	struct lws_context **pcontext_finalize;
+	const char *username, *groupname;
+
+#if defined(LWS_WITH_SERVER)
+	const char *server_string;
+#endif
+
+	struct lws_event_loop_ops *event_loop_ops;
+
+#if defined(LWS_WITH_FILE_OPS)
+	const struct lws_plat_file_ops *fops;
+#endif
+
+#if defined(LWS_WITH_TLS)
+	const struct lws_tls_ops *tls_ops;
+#endif
 #if defined(LWS_WITH_PLUGINS)
 	struct lws_plugin *plugin_list;
 #endif
@@ -297,10 +335,7 @@ struct lws_context {
 	struct lws **lws_lookup;
 
 #endif
-#endif
-#if LWS_MAX_SMP > 1
-	struct lws_mutex_refcount mr;
-#endif
+#endif /* NETWORK */
 
 #if defined(LWS_AMAZON_RTOS)
 	mbedtls_entropy_context mec;
@@ -329,36 +364,12 @@ struct lws_context {
 #endif
 	void (*eventlib_signal_cb)(void *event_lib_handle, int signum);
 
+	time_t last_ws_ping_pong_check_s;
+	lws_usec_t time_up; /* monotonic */
+
 #if defined(LWS_HAVE_SYS_CAPABILITY_H) && defined(LWS_HAVE_LIBCAP)
 	cap_value_t caps[4];
 	char count_caps;
-#endif
-
-#if defined(LWS_WITH_NETWORK)
-#if defined(LWS_WITH_LIBEV)
-	struct lws_context_eventlibs_libev ev;
-#endif
-#if defined(LWS_WITH_LIBUV)
-	struct lws_context_eventlibs_libuv uv;
-#endif
-#if defined(LWS_WITH_LIBEVENT)
-	struct lws_context_eventlibs_libevent event;
-#endif
-	struct lws_event_loop_ops *event_loop_ops;
-#endif
-
-#if defined(LWS_WITH_TLS) && defined(LWS_WITH_NETWORK)
-	struct lws_context_tls tls;
-#endif
-
-#if defined(LWS_WITH_SERVER)
-	char canonical_hostname[128];
-	const char *server_string;
-#endif
-
-#ifdef LWS_LATENCY
-	unsigned long worst_latency;
-	char worst_latency_info[256];
 #endif
 
 #if defined(LWS_PLAT_FREERTOS)

lib/plat/freertos/private-lib-plat-freertos.h
@@ -55,6 +55,9 @@ gai_strerror(int);
 
 #if defined(LWS_AMAZON_RTOS)
  #include "FreeRTOS.h"
+#if defined(LWS_WITH_SYS_ASYNC_DNS)
+ #include "FreeRTOS_IP.h"
+#endif
  #include "timers.h"
  #include <esp_attr.h>
 #else