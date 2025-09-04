4b1c4daca2411c9565c306a554cf91f98356c7e4
yangjianqing <yangjianqing@xiaomi.com>
2025-09-04 12:21:31 +0800

    libwebsockets:add type conversion
    Signed-off-by: yangjianqing <yangjianqing@xiaomi.com>

--- include/libwebsockets/lws-timeout-timer.h
+++ include/libwebsockets/lws-timeout-timer.h
@@ -114,8 +114,8 @@ void
 lws_set_timeout_us(struct lws *wsi, enum pending_timeout reason, lws_usec_t us);
 
 /* helper for clearer LWS_TO_KILL_ASYNC / LWS_TO_KILL_SYNC usage */
-#define lws_wsi_close(w, to_kill) lws_set_timeout(w, 1, to_kill)
-
+#define lws_wsi_close(w, to_kill) \
+	lws_set_timeout(w, PENDING_TIMEOUT_AWAITING_PROXY_RESPONSE, to_kill)
 
 #define LWS_SET_TIMER_USEC_CANCEL ((lws_usec_t)-1ll)
 #define LWS_USEC_PER_SEC ((lws_usec_t)1000000)
